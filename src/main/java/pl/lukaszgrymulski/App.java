3eb43cf285ca88e5c3b1783eda280e53d10d6ef1
Łukasz Grymulski <lukasz.grymulski@gmail.com>
2019-04-23 17:33:14 +0200

    implement dao to save Client in DB

--- src/main/java/pl/lukaszgrymulski/App.java
+++ src/main/java/pl/lukaszgrymulski/App.java
@@ -2,12 +2,15 @@ package pl.lukaszgrymulski;
 
 
 import pl.lukaszgrymulski.dao.ClientDao;
+import pl.lukaszgrymulski.models.Client;
 
 public class App {
 
     public static void main(String[] args) {
         ClientDao dao = new ClientDao();
-        dao.saveClientInDatabase();
+        Client client = new Client(3, "Adam", "Ondra", 25);
+        int id = dao.saveClientInDatabase(client);
+        System.out.println(id);
     }
 
 }
--- src/main/java/pl/lukaszgrymulski/dao/ClientDao.java
+++ src/main/java/pl/lukaszgrymulski/dao/ClientDao.java
@@ -1,17 +1,42 @@
 package pl.lukaszgrymulski.dao;
 
+import pl.lukaszgrymulski.models.Client;
 import pl.lukaszgrymulski.persistence.DatabaseConnection;
 
-import java.sql.Connection;
+import java.sql.*;
 
 public class ClientDao {
 
-
-
-    public void saveClientInDatabase() {
+    public int saveClientInDatabase(Client client) {
         Connection connection = DatabaseConnection.getConnection();
-        System.out.println("Gitara!");
+        Integer justMigratedClientId = null;
 
+        try {
+            String query = "INSERT INTO CLIENTS(ID, NAME, SURNAME, AGE) VALUES (null, ?, ?, ?)";
+            PreparedStatement statement = connection.prepareStatement(query, Statement.RETURN_GENERATED_KEYS);
+            statement.setString(1, client.getName());
+            statement.setString(2, client.getSurname());
+            statement.setInt(3, client.getAge());
+            int affectedRows = statement.executeUpdate();
+
+            if (affectedRows == 0) {
+                throw new SQLException("Creating client ("+client+") failed, no rows affected.");
+            }
+
+            ResultSet generatedKeys = statement.getGeneratedKeys();
+
+            if (generatedKeys.next()) {
+                justMigratedClientId =  (int) generatedKeys.getLong(1);
+            } else {
+                throw new SQLException("Creating client ("+client+") failed, no ID obtained.");
+            }
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+        System.out.println("Gitara!");
+        return justMigratedClientId;
     }
 
 }
